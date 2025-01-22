8f7059639425c7bf2f7a007c1a9356536c5b93cc
Den <den@m.com>
2025-01-22 19:33:08 +0300

    replace redis with memcached

---
+++ cache_model.py
@@ -0,0 +1,45 @@
+import asyncio
+import emcache
+import json
+
+
+def client(func):
+    async def wrapper(*args, **kwargs):
+        client = await emcache.create_client([emcache.MemcachedHostAddress('localhost', 11211)])
+        return await func(client, *args, **kwargs)
+        await client.close()
+    return wrapper
+
+async def set_data(client, key, value):
+    await client.set(key.encode('utf-8'), value.encode('utf-8'))
+
+async def get_data(client, key):
+    result = await client.get(key.encode('utf-8'))
+    if result:
+        return result.value.decode('utf-8')
+
+async def set_json(client, key, json_data):
+    await set_data(client, key, json.dumps(json_data))
+
+async def get_json(client, key):
+    data = await get_data(client, key)
+    if data:
+        return json.loads(data)
+    
+async def delete(client, key):
+    await client.delete(key.encode('utf-8'))
+
+
+async def main():
+    await client(set_data)('x1', 'abc123')
+    print(await client(get_data)('x21'))
+    print(await client(get_data)('x1'))
+    await client(delete)('x1')
+    print(await client(get_data)('x1'))
+    
+
+    await client(set_json)('x2', {'name': 'Bob'})
+    print(await client(get_json)('x2'))
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- repositories/cookie.py
+++ repositories/cookie.py
@@ -1,14 +1,14 @@
-import redis_model
-from redis_model import client
+import cache_model
+from cache_model import client
 
 @client
 async def create_cookie(client, id, cookie):
-    await redis_model.set_json(client, id, cookie)
+    await cache_model.set_json(client, id, cookie)
 
 @client
 async def get_cookie(client, id):
-    return await redis_model.get_json(client, id)
+    return await cache_model.get_json(client, id)
 
 @client
 async def delete_cookie(client, id):
-    await redis_model.delete(client, id)
+    await cache_model.delete(client, id)
