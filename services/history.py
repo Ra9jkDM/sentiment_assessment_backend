fc03688792c1dcee4c8775add813b229f7f1d558
Den <den@m.com>
2025-02-14 17:41:31 +0300

    func: return files from minio and fix bug

--- services/history.py
+++ services/history.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from repositories import history
+from repositories import history, users_storage
 
 class HistoryType(Enum):
     text = 'text'
@@ -15,3 +15,6 @@ async def get_records(username, history_type, page, amount):
 async def get_amount(username, history_type):
     if history_type == HistoryType.text:
         return await history.get_amount_of_text_records(username)
+
+async def get_file(username, _id):
+    return await users_storage.get_table(username, _id)
