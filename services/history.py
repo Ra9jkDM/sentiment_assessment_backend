77794ffc0f18cd2f8e0405b31f5bb76049a2672b
Den <den@m.com>
2025-02-26 16:19:27 +0300

    change amount records on page and conditions in repo

--- services/history.py
+++ services/history.py
@@ -5,22 +5,22 @@ class HistoryType(Enum):
     text = 'text'
     table = 'table'
 
-async def get_records(username, history_type, page, amount):
+async def get_records(username, history_type, page, amount, name, start_date, end_date):
     page = page -1
 
     if page < 0:
         page=0
 
     if history_type == HistoryType.text:
-        return await history.get_text_records(username, page, amount)
+        return await history.get_text_records(username, page, amount, name, start_date, end_date)
     elif history_type == HistoryType.table:
-        return await history.get_table_records(username, page, amount)
+        return await history.get_table_records(username, page, amount, name, start_date, end_date)
 
-async def get_amount(username, history_type):
+async def get_amount(username, history_type, name, start_date, end_date):
     if history_type == HistoryType.text:
-        return await history.get_amount_of_text_records(username)
+        return await history.get_amount_of_text_records(username, name, start_date, end_date)
     elif history_type == HistoryType.table:
-        return await history.get_amount_of_table_records(username)
+        return await history.get_amount_of_table_records(username, name, start_date, end_date)
 
 async def get_file(username, _id):
     return await users_storage.get_table(username, _id)
